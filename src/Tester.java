690aff75b5002b69a92497227ffa2d59770a1f1a
lilbroadam <lilbroadam@gmail.com>
2019-05-24 21:22:06 -0500

    Moved testing code to Tester.java

---
+++ src/Tester.java
@@ -0,0 +1,19 @@
+
+public class Tester {
+	
+	/* Checking: 20%  Savings: 60%
+	 * New car:  20%  Phone:  $30
+	 */
+	
+	public static void main(String args[]) {
+		Vault vault = new Vault("Vault 1");
+		vault.createAccount("Checking", "C", null, 20.0);
+		vault.createAccount("Savings", "S", null, 60.0);
+		vault.createAccount("New Car", "Car", null, 20.0);
+		vault.createAccount("Phone", "P", 30.0, null);
+		
+		vault.printVault();
+		
+		DistributionManager.printDistribution(new Paycheck(100), vault);
+	}
+}
