20759a0e94da9c7d03750939cca861ef1a9ab43c
lilbroadam <lilbroadam@gmail.com>
2019-05-25 12:19:50 -0500

    viewSelector loads real vaults, added mouseListener to vault buttons

--- src/PaycheckDistribution.java
+++ src/PaycheckDistribution.java
@@ -1,5 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -13,7 +17,15 @@ import javax.swing.JPanel;
 
 public class PaycheckDistribution {
 	
+	private static List<Vault> vaults;
+	
 	public static void main(String args[]) {
+		// initialize back end
+		if(vaults == null) // don't overwrite testing data
+			vaults = new ArrayList<>();
+		// read in vaults from memory
+		
+		// initialize front end
 		JFrame gui = new JFrame("Paycheck Distribution");
 		gui.setLocationRelativeTo(null); // open window in center of screen
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,7 +34,7 @@ public class PaycheckDistribution {
 		 * https://www.leepoint.net/GUI/components/menus/menus.html
 		 * https://www.javatpoint.com/java-jmenuitem-and-jmenu
 		 * 
-		 * View  Settings  Help
+		 * File  Settings  Help
 		 */
 		JMenuBar menuBar = new JMenuBar();
 			JMenu fileMenu = new JMenu("File");
@@ -38,15 +50,25 @@ public class PaycheckDistribution {
 				menuBar.add(helpMenu);
 		gui.setJMenuBar(menuBar);
 		
+		JPanel viewDetails = new JPanel();
+		viewDetails.setBorder(BorderFactory.createTitledBorder("Select a vault or paycheck to view"));
+		
 		// Set the view selector
 		JPanel viewSelector = new JPanel();
-		viewSelector.setBorder(BorderFactory.createTitledBorder("Your vaults"));
+		String viewing = "vaults";
 		viewSelector.setLayout(new BoxLayout(viewSelector, BoxLayout.Y_AXIS));
-		viewSelector.add(new JButton("Vault 1"));
-		viewSelector.add(new JButton("Vault 2"));
-		
-		JPanel viewDetails = new JPanel();
-		viewDetails.setBorder(BorderFactory.createTitledBorder("Selected vault/pc from viewSelector"));
+		viewSelector.setBorder(BorderFactory.createTitledBorder("Your " + viewing));
+		for(Vault vault : vaults) {
+			JButton vaultButton = new JButton(vault.getName());
+			vaultButton.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+					viewDetails.removeAll();
+					viewDetails.setBorder(BorderFactory.createTitledBorder(vault.getName()));
+					viewDetails.revalidate();
+				}
+			});
+			viewSelector.add(vaultButton);
+		}
 		
 		// set everything into the frame
 		Container c = gui.getContentPane();
@@ -57,6 +79,11 @@ public class PaycheckDistribution {
 //		gui.pack();
 		gui.setSize(600, 400);
 		gui.setVisible(true);
+	}
+	
+	public static void testingRun(List<Vault> vaults) {
+		PaycheckDistribution.vaults = vaults;
+		main(new String[] {});
 	}
 	
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tester {
 	
@@ -5,15 +7,27 @@ public class Tester {
 	 * New car:  20%  Phone:  $30
 	 */
 	
+	private static List<Vault> vaults;
+	
 	public static void main(String args[]) {
-		Vault vault = new Vault("Vault 1");
-		vault.createAccount("Checking", "C", null, 20.0); // TODO change nulls to 0's
-		vault.createAccount("Savings", "S", null, 60.0);
-		vault.createAccount("New Car", "Car", null, 20.0);
-		vault.createAccount("Phone", "P", 30.0, null);
+		vaults = new ArrayList<>();
 		
-		vault.printVault();
+		Vault vault1 = new Vault("Vault 1");
+		vault1.createAccount("Checking", "C", null, 20.0); // TODO change nulls to 0's
+		vault1.createAccount("Savings", "S", null, 60.0);
+		vault1.createAccount("New Car", "Car", null, 20.0);
+		vault1.createAccount("Phone", "P", 30.0, null);
 		
-		DistributionManager.printDistribution(new Paycheck(100), vault);
+		vault1.printVault();
+		
+		DistributionManager.printDistribution(new Paycheck(100), vault1);
+		
+		Vault vault2 = new Vault("Vault 2");
+		vault2.createAccount("Acc1", null, 100.0);
+		vault2.createAccount("Acc2", 10.0, null);
+		
+		vaults.add(vault1);
+		vaults.add(vault2);
+		PaycheckDistribution.testingRun(vaults);
 	}
 }
