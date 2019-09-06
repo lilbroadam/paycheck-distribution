21c03c253724ec70dead6658108d8eaa4ee56ec7
lilbroadam <lilbroadam@gmail.com>
2019-09-06 00:20:57 -0500

    Program functional enough to loosely output to console

--- src/DistributionManager.java
+++ src/DistributionManager.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
- * 
+ *
  */
 
 /* TO-DO LIST:
@@ -13,59 +15,101 @@ import java.util.List;
  */
 
 public class DistributionManager {
-	
-	public static double[] distribute(Paycheck paycheck, Vault vault) {
-		double[] result = new double[vault.numAccounts()];
-		
-		
-		
-		return result;
+
+	public static Distribution distribute(Paycheck paycheck, Vault vault) {
+		Distribution distribution = new Distribution(vault);
+
+		List<Account> accounts = vault.getAccounts();
+		Map<String, Double> distributionAmount = new HashMap<>();
+		double paycheckRemaining = paycheck.getPayAmount();
+
+		// distribute flat rates first
+		for(Account acc : accounts) {
+			// populate the distributionAmount map
+			if(!distributionAmount.containsKey(acc.getName()))
+				distributionAmount.put(acc.getName(), new Double(0.0));
+
+			// distribute the flat rate if the account has a flat rate
+			if(acc.getFlatRate() > 0) {
+				distributionAmount.put(acc.getName(), new Double(acc.getFlatRate()));
+				paycheckRemaining -= acc.getFlatRate();
+			}
+		}
+
+		// distribute perc rates second (last)
+		double percDistPoolAmount = paycheckRemaining;
+		for(Account acc : accounts) {
+
+			double percDist = percDistPoolAmount * (acc.getPercRate() / 100.0);
+
+			distributionAmount.put(acc.getName(),
+				new Double(distributionAmount.get(acc.getName()) + percDist)
+			);
+
+			paycheckRemaining -= percDist;
+		}
+
+		// TODO check for arithmatic/rounding errors
+		System.out.println(paycheckRemaining == 0.0 ? "No remainder left over" : "remainder: " + paycheckRemaining);
+
+		// TODO delete temporary console output
+		for(String acc : distributionAmount.keySet())
+			System.out.println(acc + ": " + distributionAmount.get(acc));
+
+		return new Distribution(vault); // TODO maybe should just return a regular Map?
 	}
-	
+
 	// print the distribution amounts for the given paycheck into the given vault
 	public static void printDistribution(Paycheck paycheck, Vault vault) {
-		final double PAYCHECK_AMOUNT = paycheck.getPayAmmount();
+		final double PAYCHECK_AMOUNT = paycheck.getPayAmount();
 		double paycheckRemaining = PAYCHECK_AMOUNT;
 		double totalDistributed = 0.00;
-		
-		
+
+
 		List<Account> accounts = vault.getAccounts();
 		double[] accountDistribution = new double[accounts.size()];
-		
+
 		// flat rates are applied first
 		for(int i = 0; i < accountDistribution.length; i++) {
 			double distributionAmount = accounts.get(i).getFlatRate();
-			
+
 			accountDistribution[i] += distributionAmount;
 			paycheckRemaining -= distributionAmount;
 			totalDistributed += distributionAmount;
 		}
-		
+
 		// percentage rates are applied last
 		double amountForPerc = paycheckRemaining;
 		for(int i = 0; i < accountDistribution.length; i++) {
 			double distributionPercentage = accounts.get(i).getPercRate() / 100.0; // TODO round down to 2 decimal places
 			double distributionAmount = amountForPerc * distributionPercentage;
-			
+
 			accountDistribution[i] += distributionAmount;
 			paycheckRemaining -= distributionAmount;
 			totalDistributed += distributionAmount;
 		}
-		
+
 		// print
-		
+
 		System.out.println("$" + PAYCHECK_AMOUNT + " into " + vault.getName() + ":");
 		System.out.println("----------");
-		
+
 		for(int i = 0; i < accountDistribution.length; i++) {
 			System.out.printf("%" + Account.MAX_ABBRV_LENGTH + "s: $%f\n", accounts.get(i).getAbbreviation(), accountDistribution[i]);
 		}
-		
+
 		// print total distributed
 		System.out.println("----------");
 		System.out.println("Amount distributed: " + totalDistributed);
 		// display any leftovers and ask the user how they want them to be distributed
 		System.out.println("----------");
 		System.out.println("Leftovers: " + paycheckRemaining);
+	}
+}
+
+class Distribution {
+
+	public Distribution(Vault vault) {
+
 	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,38 +1,53 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Tester {
-	
+
 	/* Checking: 20%  Savings: 60%
 	 * New car:  20%  Phone:  $30
 	 */
-	
+
 	private static List<Vault> vaults;
-	
+
 	public static void main(String args[]) {
+		Vault vault = new Vault("Vault 1");
+		vault.createAccount("Checking", "C", 10.00, 40.00);
+		vault.createAccount("Savings", "GS", 20.00, 60.00);
+
+		System.out.print("Paycheck amount: ");
+		Paycheck paycheck = new Paycheck(new Scanner(System.in).nextDouble()); // FIXME scanner temp input
+		System.out.println();
+
+		DistributionManager.distribute(paycheck, vault); // TODO change to Distributer.distribute(...)
+
+		// testGUI();
+	}
+
+	private static void testGUI(){
 		vaults = new ArrayList<>();
-		
+
 		Vault vault1 = new Vault("Vault 1");
 		vault1.createAccount("Checking", "C", null, 20.0); // TODO change nulls to 0's
 		vault1.createAccount("Savings", "S", null, 60.0);
 		vault1.createAccount("New Car", "Car", null, 20.0);
 		vault1.createAccount("Phone", "P", 30.0, null);
-		
+
 		vault1.printVault();
-		
+
 		DistributionManager.printDistribution(new Paycheck(100), vault1);
-		
+
 		Vault vault2 = new Vault("Vault 2");
 		vault2.createAccount("Acc1", null, 100.0);
 		vault2.createAccount("Acc2", 10.0, null);
-		
+
 		vaults.add(vault1);
 		vaults.add(vault2);
-		
+
 		PaycheckDistribution.testingRun(vaults);
 	}
-	
+
 	private static void testViewSelectorScroll() {
 		for(int i = 3; i <= 20; i++)
 			vaults.add(new Vault("Vault " + i));
